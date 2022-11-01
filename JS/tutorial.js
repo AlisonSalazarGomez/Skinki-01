efa4852fc1468bd6df28a55034f1a11e99cfb7fd
Angie <garay.s.mariaangelica@gmail.com>
2022-10-31 22:37:53 -0300

    Empecé a utilizar JS

---
+++ JS/tutorial.js
@@ -0,0 +1,10 @@
+var titulo=document.getElementById("titulo")
+var tutorial=localStorage.getItem("tutorial")
+var video=document.getElementById("video")
+titulo.textContent="tutorial "+tutorial
+var tutorialesVideo={
+principiante:`<iframe width="560" height="315" src="https://www.youtube.com/embed/BSTyuQV2nBw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+intermedio:`<iframe width="560" height="315" src="https://www.youtube.com/embed/BSTyuQV2nBw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+avanzado:`<iframe width="560" height="315" src="https://www.youtube.com/embed/BSTyuQV2nBw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+}
+video.innerHTML=tutorialesVideo[tutorial]
